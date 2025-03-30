1fbf832c10912ed848541717f1b7e1e17fe6ad14
pabloamanda404@gmail.com <pabloamanda404@gmail.com>
2025-03-30 13:17:57 +0000

    “一个简单的shareptr”

--- include/stl/list.h
+++ include/stl/list.h
@@ -1,6 +1,6 @@
 #pragma once
-namespace stl {
 
+namespace stl {
 
     template <class T> class NodeBase;
     template <class T> class NodeData;
@@ -8,8 +8,8 @@ namespace stl {
     template <class T>
     class NodeBase {
     public:
-        typedef  NodeBase<T>* BasePtr;
-        typedef  NodeData<T>* NodePtr;
+        typedef NodeBase<T>* BasePtr;
+        typedef NodeData<T>* NodePtr;
 
     public:
         BasePtr forward;
@@ -19,14 +19,13 @@ namespace stl {
     public:
         NodeBase() : forward(nullptr), next(nullptr), ptr(nullptr) {};
         NodeBase(NodePtr _ptr) : forward(nullptr), next(nullptr), ptr(_ptr) {};
-        NodeBase(BasePtr other) :forward(other->forward), next(other->next), ptr(other->ptr) {};
+        NodeBase(BasePtr other) : forward(other->forward), next(other->next), ptr(other->ptr) {};
 
     public:
         void operator++(int) { *this = *next; };
         bool operator==(const NodeBase<T>& other) { return forward == other.forward && next == other.next && ptr == other.ptr; };
         T& operator*() { return ptr->data; };
         T* operator->() { return &(ptr->data); };
-
     };
 
     template <class T>
@@ -39,24 +38,83 @@ namespace stl {
         NodeData(const T& _data) : data(_data), NodeBase<T>(this) {};
     };
 
-
-
-    //TODO:list暂时不实现，后续再补充
-    template<class T>
+    // list class implementation
+    template <class T>
     class list {
     public:
         typedef NodeBase<T> iterator;
         typedef NodeBase<T>* pointer;
+        typedef NodeData<T> node_data;
 
     public:
         pointer begin;
         pointer end;
+        size_t size;
 
     public:
-        list();
+        list() : begin(nullptr), end(nullptr), size(0) {}
 
+        // Check if the list is empty
+        bool empty() const { return size == 0; }
+
+        // Add a new element to the end
+        void push_back(const T& value) {
+            pointer new_node = new node_data(value);
+            if (empty()) {
+                begin = new_node;
+                end = new_node;
+            }
+            else {
+                end->next = new_node;
+                new_node->forward = end;
+                end = new_node;
+            }
+            ++size;
+        }
+
+        // Remove the element from the end
+        void pop_back() {
+            if (empty()) return;
+            pointer temp = end;
+            end = end->forward;
+            if (end) {
+                end->next = nullptr;
+            }
+            else {
+                begin = nullptr;
+            }
+            delete temp;
+            --size;
+        }
+
+        // Access the front element
+        T& front() {
+            if (empty()) throw std::out_of_range("List is empty");
+            return begin->ptr->data;
+        }
+
+        // Access the back element
+        T& back() {
+            if (empty()) throw std::out_of_range("List is empty");
+            return end->ptr->data;
+        }
+
+        // Clear the entire list
+        void clear() {
+            while (!empty()) {
+                pop_back();
+            }
+        }
+
+        // Destructor to clean up nodes
+        ~list() { clear(); }
+
+        // Iterator to begin
+        iterator begin() { return *this->begin; }
+
+        // Iterator to end
+        iterator end() { return *this->end; }
 
     };
-
 
 }
--- include/stl/shared_ptr.h
+++ include/stl/shared_ptr.h
@@ -1,7 +1,47 @@
 #pragma once
 
 
+template <typename T>
+class SharedPtr {
+private:
+    T* ptr;
+    int* ref_count;
 
-class shared_ptr {
+public:
+    explicit SharedPtr(T* p = nullptr) : ptr(p), ref_count(new int(1)) {}
 
+    SharedPtr(const SharedPtr& other) : ptr(other.ptr), ref_count(other.ref_count) {
+        if (ptr) {
+            ++(*ref_count);
+        }
+    }
+
+    SharedPtr& operator=(const SharedPtr& other) {
+        if (this != &other) {
+            release();
+            ptr = other.ptr;
+            ref_count = other.ref_count;
+            if (ptr) {
+                ++(*ref_count);
+            }
+        }
+        return *this;
+    }
+
+    ~SharedPtr() {
+        release();
+    }
+
+    T& operator*() const { return *ptr; }
+    T* operator->() const { return ptr; }
+    T* get() const { return ptr; }
+    int use_count() const { return *ref_count; }
+
+private:
+    void release() {
+        if (ptr && --(*ref_count) == 0) {
+            delete ptr;
+            delete ref_count;
+        }
+    }
 };
