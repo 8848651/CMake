5cc544ecab96829e0f913892ccf0d7e34712941e
8848651 <89018515+8848651@users.noreply.github.com>
2026-07-31 17:36:21 +0000

    " "

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,6 @@
 #include <complex>
 
 
-auto FFT(){
-    
-}
-
-
-
 
 int main() {
     constexpr static size_t tp = 8;
@@ -30,45 +24,42 @@ int main() {
 
     std::array<std::complex<double>, tp> arr;
     std::array<std::complex<double>, tp> arr_t;
+    std::array<std::complex<double>, tp> arr_f;
     for(int i=0;i<tp;i++){
         auto temp=dis(gen);
         arr[i]=std::complex<double>(temp, 0);
         arr_t[i]=std::complex<double>(temp, 0);
+        arr_f[i]=std::complex<double>(temp, 0);
         std::cout<< arr[i] <<" ";
     }
     std::cout<<std::endl;
 
     stl::dit_butterfly_step<tp,0,1>(arr_t);
+    stl::dif_butterfly_step<tp,0>(arr_f);
 
 
     for(int i=0;i < tp; i++){
         if(temp[i] >= i){
             std::swap(arr[temp[i]] , arr[i]);
             std::swap(arr_t[temp[i]] , arr_t[i]);
+            std::swap(arr_f[temp[i]] , arr_f[i]);
         }
     }
 
-    for(int len=2;len<=tp;len=len*2){
-        std::complex<double> wn = std::polar(1.0, -2*M_PI/len);
-        for(int i=0;i<tp/len;i++){
-            std::complex<double> w = 1.0;
-            for(int j=i*len;j<i*len+len/2;j++){
-                auto a = arr[j];
-                auto b = arr[j+len/2];
-                arr[j] = a + b * w;
-                arr[j+len/2] = a - b * w;
-                w *= wn;
-            }
-        }
-    }
+    stl::dit_butterfly_iterate(arr);
 
     for(int i=0;i<tp;i++){
         std::cout<<arr[i]<<" ";
     }
     std::cout<<std::endl;
 
-        for(int i=0;i<tp;i++){
+    for(int i=0;i<tp;i++){
         std::cout<<arr_t[i]<<" ";
+    }
+    std::cout<<std::endl;
+
+    for(int i=0;i<tp;i++){
+        std::cout<<arr_f[i]<<" ";
     }
     std::cout<<std::endl;
 
--- stl/include/stl/fft.h
+++ stl/include/stl/fft.h
@@ -5,6 +5,25 @@
 #include <complex>
 namespace stl {
 
+    //迭代法
+    template<typename T, size_t U>
+    void dit_butterfly_iterate(std::array<std::complex<T>, U>& array){
+        size_t size=array.size();
+        for(int len=2;len<=size;len=len*2){
+            std::complex<T> wn = std::polar(1.0, -2*M_PI/len);
+            for(int i=0;i<size/len;i++){
+                std::complex<T> w = 1.0;
+                for(int j=i*len;j<i*len+len/2;j++){
+                    auto a = array[j];
+                    auto b = array[j+len/2];
+                    array[j] = a + b * w;
+                    array[j+len/2] = a - b * w;
+                    w *= wn;
+                }
+            }
+        }
+    }
+
     //Q为一次FFT数组的长度，M为起始地址，N为跨度
     template<size_t Q,size_t M,size_t N,typename T, size_t U>
     typename stl::void_type<Q==2>::type dit_butterfly_step(std::array<std::complex<T>, U>& array){
@@ -23,7 +42,7 @@ namespace stl {
     
         for (size_t i = M; i < array.size(); i = i + N * 2) {
             size_t r=(i-M)/(2*N);
-            std::complex<double> w = std::polar(1.0, -2.0 * M_PI * temp[r] / (U / N));
+            std::complex<T> w = std::polar(1.0, -2.0 * M_PI * temp[r] / (U / N));
 
             auto datae = array[i];
             auto datao = array[i + N];
@@ -31,6 +50,33 @@ namespace stl {
             array[i + N] = datae - datao * w;
         }
     };
+
+
+
+    //Q 为FFT长度 M为起始位置
+    template<size_t Q,size_t M,typename T,size_t U>
+    typename stl::void_type<Q==2>::type dif_butterfly_step(std::array<std::complex<T>, U>& array){
+        auto datae = array[M];
+        auto datao = array[M+1];
+        array[M] = datae + datao;
+        array[M+1] = datae - datao;
+    }
+
+    template<size_t Q,size_t M,typename T,size_t U>
+    typename stl::void_type<Q!=2>::type dif_butterfly_step(std::array<std::complex<T>, U>& array){
+        std::complex<double> wn = std::polar(1.0,-2.0*M_PI/Q);
+        std::complex<double> w = 1.0;
+        for(int i=M;i<M+Q/2;i++){
+            auto datae = array[i];
+            auto datao = array[i+Q/2];
+            array[i] = datae + datao;
+            array[i+Q/2] = (datae - datao)*w;
+            w*=wn;
+        }
+        dif_butterfly_step<Q/2,M>(array);
+        dif_butterfly_step<Q/2,M+Q/2>(array);
+    }
+
 
 
 }
